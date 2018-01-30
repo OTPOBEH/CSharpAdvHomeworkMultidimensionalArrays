9553d010699e15ab11002948c3f3c384a9a6ee56
OTPOBEH <3akobah@gmail.com>
2018-01-30 20:03:24 +0200

    Solved...

--- 01.MatrixOfPalindromes/MatrixOfPalindromes.cs
+++ 01.MatrixOfPalindromes/MatrixOfPalindromes.cs
@@ -1,15 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 class MatrixOfPalindromes
 {
     static void Main()
     {
+        var alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
 
+        var size = Console.ReadLine()?.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
 
+        var rows = size[0];
+        var columns = size[1];
+        var matrix = new string[rows, columns];
 
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < columns; col++)
+            {
+                matrix[row, col] = new string(alphabet[row],1) + new string(alphabet[row + col],1) + new string(alphabet[row],1);
+
+                Console.Write(matrix[row, col] + " ");
+            }
+            Console.WriteLine();
+        }
     }
 }
