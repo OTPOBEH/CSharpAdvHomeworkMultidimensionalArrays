6fbbba20bd3ae8876557914c4f77fdf271c2cc49
Stefan Petrov <3akobah@gmail.com>
2018-01-31 09:21:02 +0200

    Problem 05 started...

---
+++ 05.RubiksMatrix/RubiksMatrix.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class RubiksMatrix
+{
+    static void Main()
+    {
+        var size = Console.ReadLine()?.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+        var rows = size[0];
+        var columns = size[1];
+        var current = 0;
+        var matrix = new int[rows, columns];
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < columns; col++)
+            {
+                matrix[row, col] = ++current;
+            }
+        }
+
+
+
+
+
+
+    }
+}
