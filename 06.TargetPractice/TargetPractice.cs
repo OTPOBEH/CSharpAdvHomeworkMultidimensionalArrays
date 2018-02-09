fc16108408ef81d44906c58ac14bc945501c1094
OTPOBEH <3akobah@gmail.com>
2018-02-09 18:05:07 +0200

    Update....

---
+++ 06.TargetPractice/TargetPractice.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+
+namespace TargetPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToArray();
+            var rows = int.Parse(size[0]);
+            var cols = int.Parse(size[1]);
+
+            var wall = new char[rows, cols];
+            var snake = Console.ReadLine().ToCharArray();
+            var isEven = true;
+            var counter = 0;
+
+            for (int i = rows - 1; i >= 0; i--)
+            {
+                if (isEven)
+                {
+                    for (int j = cols - 1; j >= 0; j--)
+                    {
+                        wall[i, j] = snake[counter % snake.Length];
+                        counter++;
+                    }
+                    isEven = false;
+                }
+                else
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        wall[i, j] = snake[counter % snake.Length];
+                        counter++;
+                    }
+                    isEven = true;
+                }
+            }
+
+            var shot = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var impactRow = shot[0];
+            var impactCol = shot[1];
+            var radius = shot[2];
+
+            var topRow = impactRow - radius;
+            var bottomRow = impactRow + radius;
+            var startCol = impactCol;
+            counter = 0;
+
+            for (int row = topRow; row <= impactRow; row++)
+            {
+                for (int col = startCol; row >= 0 && col <= startCol + counter; col++)
+                {
+                    if (col < 0 || col >= wall.GetLength(1)) continue;
+                    wall[row, col] = '\0';
+                }
+                startCol--;
+                counter += 2;
+            }
+
+            counter = 2 * radius - 1;
+            startCol += 2;
+
+            for (int row = impactRow + 1; row < wall.GetLength(0) && row <= bottomRow; row++)
+            {
+                for (int col = startCol; row < wall.GetLength(1) && col < startCol + counter; col++)
+                {
+                    if (col < 0 || col >= wall.GetLength(1)) continue;
+                    wall[row, col] = '\0';
+                }
+                startCol++;
+                counter -= 2;
+            }
+
+
+
+
+            Console.WriteLine();
+
+        }
+    }
+}
