6e6ca41bb374f90179355d064c6f8a1ffa38b246
Stefan Petrov <3akobah@gmail.com>
2018-02-11 12:45:15 +0200

    Update...

--- 06.TargetPractice/TargetPractice.cs
+++ 06.TargetPractice/TargetPractice.cs
@@ -1,18 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace TargetPractice
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var size = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToArray();
+            var size = Console.ReadLine()?.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToArray();
             var rows = int.Parse(size[0]);
             var cols = int.Parse(size[1]);
 
             var wall = new char[rows, cols];
-            var snake = Console.ReadLine().ToCharArray();
+            var snake = Console.ReadLine()?.ToCharArray();
             var isEven = true;
             var counter = 0;
 
@@ -38,7 +39,7 @@ namespace TargetPractice
                 }
             }
 
-            var shot = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var shot = Console.ReadLine()?.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
             var impactRow = shot[0];
             var impactCol = shot[1];
             var radius = shot[2];
@@ -53,7 +54,7 @@ namespace TargetPractice
                 for (int col = startCol; row >= 0 && col <= startCol + counter; col++)
                 {
                     if (col < 0 || col >= wall.GetLength(1)) continue;
-                    wall[row, col] = '\0';
+                    wall[row, col] = ' ';
                 }
                 startCol--;
                 counter += 2;
@@ -67,17 +68,39 @@ namespace TargetPractice
                 for (int col = startCol; row < wall.GetLength(1) && col < startCol + counter; col++)
                 {
                     if (col < 0 || col >= wall.GetLength(1)) continue;
-                    wall[row, col] = '\0';
+                    wall[row, col] = ' ';
                 }
                 startCol++;
                 counter -= 2;
             }
 
+            var printArray = new List<char>[cols];
 
+            for (int col = 0; col < cols; col++)
+            {
+                printArray[col] = new List<char>();
+                for (int row = rows - 1; row >= 0; row--)
+                {
+                    if (wall[row, col] != ' ')
+                    {
+                        printArray[col].Add(wall[row, col]);
+                    }
+                }
 
+                while (printArray[col].Count != cols)
+                {
+                    printArray[col].Add(' ');
+                }
+            }
 
-            Console.WriteLine();
-
+            for (int row = rows - 1; row >= 0; row--)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(printArray[col].ElementAt(row));
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
