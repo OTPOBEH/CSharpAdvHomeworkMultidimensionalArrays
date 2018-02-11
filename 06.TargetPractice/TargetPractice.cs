0b121932c00c55f4515eb52855af308a9a4548fe
Stefan Petrov <3akobah@gmail.com>
2018-02-11 14:54:58 +0200

    Update...

--- 06.TargetPractice/TargetPractice.cs
+++ 06.TargetPractice/TargetPractice.cs
@@ -65,7 +65,7 @@ namespace TargetPractice
 
             for (int row = impactRow + 1; row < wall.GetLength(0) && row <= bottomRow; row++)
             {
-                for (int col = startCol; row < wall.GetLength(1) && col < startCol + counter; col++)
+                for (int col = startCol; row < wall.GetLength(1) && col < startCol + counter; col++)// грешка...
                 {
                     if (col < 0 || col >= wall.GetLength(1)) continue;
                     wall[row, col] = ' ';
@@ -73,23 +73,26 @@ namespace TargetPractice
                 startCol++;
                 counter -= 2;
             }
-
-            var printArray = new List<char>[cols];
-
-            for (int col = 0; col < cols; col++)
+            for (int col = cols - 1; col >= 0; col--)
             {
-                printArray[col] = new List<char>();
+                var tempList = new List<char>();
+
                 for (int row = rows - 1; row >= 0; row--)
                 {
                     if (wall[row, col] != ' ')
                     {
-                        printArray[col].Add(wall[row, col]);
+                        tempList.Add(wall[row, col]);
                     }
                 }
 
-                while (printArray[col].Count != cols)
+                while (tempList.Count != cols)
                 {
-                    printArray[col].Add(' ');
+                    tempList.Add(' ');
+                }
+
+                for (int row = 0; row < rows; row++)
+                {
+                    wall[row, col] = tempList[row];
                 }
             }
 
@@ -97,7 +100,7 @@ namespace TargetPractice
             {
                 for (int col = 0; col < cols; col++)
                 {
-                    Console.Write(printArray[col].ElementAt(row));
+                    Console.Write(wall[row, col]);
                 }
                 Console.WriteLine();
             }
